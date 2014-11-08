3f5d4f278b0f06e9ca37c10d57f75ed564bd500e
opentutor <opentutor@163.com>
2014-11-08 22:40:25 +0800

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int /*argc*/, char */*argv*/[])
+{
+    string welcome("Welcome to GIT!\n");
+    cout << welcome << endl;
+
+    return 0;
+}
